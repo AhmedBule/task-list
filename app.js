252b1ec8dba1af075b212876760bbe7aa1868d3b
Ahmed Bule <ahmedbule01@gmail.com>
2020-05-20 19:32:18 -0500

    added evenlisteners and values

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ let form = document.querySelector("#task-form");    // For an ID, we use a hash(
 let taskList = document.querySelector(".collection");   // For a class, we a dot(.)
 let filter = document.querySelector("#filter");
 let taskInput = document.querySelector("#task");
+
+
+// load all event listener
+
+loadEventlisteners();
+
+// Load all event listeners
+function loadEventlisteners(){
+    // Add task event
+    form.addEventListener("submit", addTask)
+}
+
+// Add task
+function addTask(e) {
+    if (taskInput.value === ''){
+        alert("Add a task");
+    }
+
+
+    e.preventDefault()
+}
