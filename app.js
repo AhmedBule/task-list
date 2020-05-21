7f7d4f2495b8dd46ead63d4c67e6f05076be4058
Ahmed Bule <ahmedbule01@gmail.com>
2020-05-20 19:25:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// We have to define UI vars
+
+let form = document.querySelector("#task-form");    // For an ID, we use a hash(#)
+let taskList = document.querySelector(".collection");   // For a class, we a dot(.)
+let filter = document.querySelector("#filter");
+let taskInput = document.querySelector("#task");
